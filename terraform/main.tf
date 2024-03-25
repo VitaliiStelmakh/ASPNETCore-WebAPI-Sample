254637958759c118941425e0c3473dad71b9f76c
Віталій Стельмах <vitalistelmakh30@gmail.com>
2024-03-25 11:44:11 +0200

    add tf

---
+++ terraform/main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.94.0 "
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.6.0"}
+  }  
+  backend "azurerm" {
+    resource_group_name   = "tfstate"
+    storage_account_name  = "tfstatedocker123"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+# Resource Group
+resource "azurerm_resource_group" "bestrong_docker_rg" {
+  name     = "bestrong_docker_rg"
+  location = "eastus"
+}
+
+# App Service Plan
+resource "azurerm_service_plan" "be_strong_docker_asp" {
+  name                = "beStrongDockerAppServicePlan"
+  location            = azurerm_resource_group.bestrong_docker_rg.location
+  resource_group_name = azurerm_resource_group.bestrong_docker_rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+
+# App Service
+resource "azurerm_linux_web_app" "be_strong_docker_as" {
+  name                = "beStrongDockerAppService"
+  location            = azurerm_resource_group.bestrong_docker_rg.location
+  resource_group_name = azurerm_resource_group.bestrong_docker_rg.name
+  service_plan_id = azurerm_service_plan.be_strong_docker_asp.id
+  site_config {} 
+}
+
+
+# Random String
+resource "random_string" "acr_suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+# Container Registry
+resource "azurerm_container_registry" "be_strong_docker_acr" {
+  name                     = "beStrongDockerAcr${random_string.acr_suffix.result}"
+  resource_group_name      = azurerm_resource_group.bestrong_docker_rg.name
+  location                 = azurerm_resource_group.bestrong_docker_rg.location
+  sku                      = "Standard"
+  admin_enabled            = false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
